a045892136d3c0a0eed6fff689a059ad3fa61e65
cradich <radichc@gmail.com>
2017-07-24 18:50:56 -0500

    added link constraint component and a matrix4x4 property drawer. updated project to use .NET 4.6. ignoring VS project files

---
+++ Assets/Source/Components/LinkConstraint.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+
+[DisallowMultipleComponent]
+[ExecuteInEditMode]
+public class LinkConstraint : MonoBehaviour
+{
+
+	[SerializeField]
+	private Transform linkedTransform;
+
+	[SerializeField]
+	private bool linkPosition = true;
+
+	[SerializeField]
+	private bool linkRotation = true;
+
+	//[SerializeField]
+	//private bool linkScale = true;
+
+	private Matrix4x4 linkedParentTRS;
+
+	private Matrix4x4 worldTRS;
+
+	private Matrix4x4 localTRS;
+
+	private Matrix4x4 linkedTRS;
+
+	private void Start()
+	{
+		UpdateLastTransform();
+		linkedTRS = linkedParentTRS.inverse * worldTRS;
+	}
+
+	private void Update()
+	{
+		if(linkedTransform != null)
+		{
+			ApplyLinkedTransform();
+		}
+	}
+
+	private void ApplyLinkedTransform()
+	{
+		//Matrix4x4 currentLocalTRS = Matrix4x4.TRS(transform.localPosition, transform.localRotation, transform.localScale);
+		Matrix4x4 oldLocalTRS = localTRS;
+
+		localTRS.SetTRS(transform.localPosition, transform.localRotation, transform.localScale);
+		worldTRS = transform.localToWorldMatrix;
+		linkedParentTRS = linkedTransform.localToWorldMatrix;
+
+		if(oldLocalTRS != localTRS)
+		{
+			linkedTRS = linkedParentTRS.inverse * worldTRS;
+		}
+
+
+		Matrix4x4 newTRS = linkedParentTRS * linkedTRS;
+
+
+		if(linkPosition)
+		{
+			transform.position = newTRS.GetColumn(3);
+		}
+
+		if(linkRotation)
+		{
+			transform.rotation = Quaternion.LookRotation(newTRS.GetColumn(2), newTRS.GetColumn(1));
+		}
+
+		// TODO figure out how unity handles scaling
+		//if(linkScale)
+		//{
+		//	transform.localScale = new Vector3(newTRS.GetColumn(0).magnitude, newTRS.GetColumn(1).magnitude, newTRS.GetColumn(2).magnitude);
+		//}
+
+		localTRS.SetTRS(transform.localPosition, transform.localRotation, transform.localScale);
+
+	}
+
+	private void UpdateLastTransform()
+	{
+		worldTRS = transform.localToWorldMatrix;
+		localTRS.SetTRS(transform.localPosition, transform.localRotation, transform.localScale);
+
+		if(linkedTransform != null)
+		{
+			linkedParentTRS = linkedTransform.localToWorldMatrix;
+		}
+	}
+}
---
+++ Assets/Source/Editor/CustomDrawers/Matrix4x4Drawer.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(Matrix4x4))]
+public class Matrix4x4Drawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		position.height = EditorGUIUtility.singleLineHeight;
+		property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, label);
+		position.y += position.height;
+
+		if(property.isExpanded)
+		{
+			EditorGUI.indentLevel++;
+
+			float windowWidth = position.width;
+			float startX = position.x;
+
+			position.width = windowWidth / 4f;
+
+			for(int c = 0; c < 4; c++)
+			{
+				for(int r = 0; r < 4; r++)
+				{
+					property.Next(true);
+					property.floatValue = EditorGUI.FloatField(position, property.floatValue);
+					position.x += position.width;
+				}
+				position.x = startX;
+				position.y += position.height;
+			}
+
+			EditorGUI.indentLevel--;
+		}
+	}
+
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		float result = 0;
+		float lineHeight = EditorGUIUtility.singleLineHeight;
+		result += lineHeight;
+
+		if(property.isExpanded)
+		{
+			for (int c = 0; c < 4; c++)
+			{
+				for (int r = 0; r < 4; r++)
+				{
+					property.Next(true);
+				}
+				result += lineHeight;
+			}
+		}
+
+		return result;
+	}
+}
