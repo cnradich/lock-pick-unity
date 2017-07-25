01c8d77ef05620e88a892047b7224e6ca4343257
cradich <radichc@gmail.com>
2017-07-25 16:02:42 -0500

    added rangefloat struct and editor

---
+++ Assets/Source/Editor/CustomDrawers/RangeFloatDrawer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(RangeFloat))]
+public class RangeFloatDrawer : PropertyDrawer
+{
+	private static GUIContent[] emptyContent = new GUIContent[2] { new GUIContent(" "), new GUIContent(" ") };
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		position.height = EditorGUIUtility.singleLineHeight;
+
+		SerializedProperty r1Prop = property.FindPropertyRelative("r1");
+		SerializedProperty r2Prop = property.FindPropertyRelative("r2");
+		float[] r = new float[2] { r1Prop.floatValue, r2Prop.floatValue };
+
+		EditorGUI.BeginChangeCheck();
+		EditorGUI.MultiFloatField(position, label, emptyContent, r);
+		if(EditorGUI.EndChangeCheck())
+		{
+			r1Prop.floatValue = r[0];
+			r2Prop.floatValue = r[1];
+		}
+	}
+
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		return EditorGUIUtility.singleLineHeight;
+	}
+}
---
+++ Assets/Source/RangeFloat.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+using UnityEngine;
+
+
+[Serializable]
+public struct RangeFloat
+{
+
+	[SerializeField]
+	private float r1;
+
+	[SerializeField]
+	private float r2;
+
+	public RangeFloat(float value1, float value2)
+	{
+		r1 = value1;
+		r2 = value2;
+	}
+
+	public float Min
+	{
+		get
+		{
+			return r1 < r2 ? r1 : r2;
+		}
+
+		set
+		{
+			if(r1 < r2)
+			{
+				r1 = value;
+			}
+			else
+			{
+				r2 = value;
+			}
+		}
+	}
+
+	public float Max
+	{
+		get
+		{
+			return r1 > r2 ? r1 : r2;
+		}
+
+		set
+		{
+			if (r1 > r2)
+			{
+				r1 = value;
+			}
+			else
+			{
+				r2 = value;
+			}
+		}
+	}
+
+	public float Length
+	{
+		get
+		{
+			return Max - Min;
+		}
+
+		set
+		{
+			Max = Min + value;
+		}
+	}
+
+	public override string ToString()
+	{
+		return String.Format("Range: {{0}, {1}}", Min, Max);
+	}
+}
